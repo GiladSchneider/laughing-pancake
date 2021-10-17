d1266c0d9fe0fa4d5a83608c8af480a5d419ee64
GiladSchneider <73044725+GiladSchneider@users.noreply.github.com>
2021-10-16 21:59:55 -0400

    NOW it works....probably (please)

--- repeated_astar.py
+++ repeated_astar.py
@@ -12,10 +12,13 @@ def xy_to_state(state_x, state_y, n):
 def reveal(state_x, state_y, game_board, board, n):
     if (state_x > 0) and (game_board[state_x-1][state_y] != 'X'):
         game_board[state_x-1][state_y] = board[state_x-1][state_y]
+    
     if (state_x < (n-1)) and (game_board[state_x+1][state_y] != 'X'):
        game_board[state_x+1][state_y] = board[state_x+1][state_y]
+    
     if (state_y > 0) and (game_board[state_x][state_y-1] != 'X'):
         game_board[state_x][state_y-1] = board[state_x][state_y-1]
+    
     if (state_y < (n-1)) and (game_board[state_x][state_y+1] != 'X'):
         game_board[state_x][state_y+1] = board[state_x][state_y+1]
 
@@ -43,6 +46,7 @@ def r_astar(board, xstart, ystart, xtar, ytar, n, num):
 
         # If no path exists, stop
         if path == None:
+            game_board[xstart][ystart], game_board[goal_x][goal_y] = 'S', 'T'
             f = open("arrays%a/arrays%sansR.txt" %(n,num),"w")
             f.write("No path found"+"\n")
             f.write("board "+str(num)+" Original"+"\n")
@@ -54,20 +58,24 @@ def r_astar(board, xstart, ystart, xtar, ytar, n, num):
             found = True
             return game_board
 
-        for s in reversed(path):
+        for s in reversed(list(range(len(path)))):
             # Reveal current location and calculate next move
             reveal(state_x, state_y, game_board, board, n)
             to_x, to_y = state_to_xy(path[s], n)
             
+            # if to_x == state_x and to_y == state_y:
+            #     print('ERR 1')
+            #     continue
+
             # If your next move is obstructed, recalculate the path
             if board[to_x][to_y] == 'B':
+                #print('ERR 2')
                 path = loop(copy.deepcopy(game_board), state_x, state_y, xtar, ytar, n, num)
                 break
             
             # If your path is not obstructed, take a step
             else:
                 state_x, state_y = to_x, to_y
-                
                 # Check for a win
                 if state_x == goal_x and state_y == goal_y:
                     print("Success!")
@@ -76,6 +84,7 @@ def r_astar(board, xstart, ystart, xtar, ytar, n, num):
                     break
                 else:
                     game_board[state_x][state_y] = 'X'
+                    #
                 
     f = open("arrays%a/arrays%sansR.txt" %(n,num),"w")
     f.write("Path found"+"\n")
