f7145537af397661e54de76ef95265ed5cfe1361
GiladSchneider <73044725+GiladSchneider@users.noreply.github.com>
2021-10-20 13:32:10 -0400

    done

--- maze_visualizer.py
+++ maze_visualizer.py
@@ -15,6 +15,21 @@ def vis_init_r(n,num):
 
     return board,n,num,message
 
+def vis_init_r_b(n,num):
+    board=[]
+    file = open("arrays%a/arrays%sRbackwards.txt" %(n,num),"r")
+    
+    message = file.readline()
+    for j in range(104):
+        file.readline()
+
+    for i in range(n):
+        line = file.readline()
+        board.append(list(line[:-1]))
+    file.close()
+
+    return board,n,num,message
+
 def vis_init_s(n,num):
     board=[]
 
@@ -26,11 +41,11 @@ def vis_init_s(n,num):
 
     return board,n,num
 
-def visualize(board, n, num, message = None):
+def visualize(board, n, num, message = None, type = 0):
     for i in range(n):
         for j in range(n):
             letter = board[i][j]
-            swap = '🟢'
+            swap = '+'
             if letter == 'O':
                 swap = '*'
             elif letter == 'B':
@@ -44,19 +59,30 @@ def visualize(board, n, num, message = None):
             elif letter == 'T':
                 swap = 'T'
             board[i][j] = swap
-    if message != None:
-        f = open("Visualized/arrays%srepeated_astar.txt" %(num),"w")
-        f.write(message)
-    else:
+    if type == 1:
         f = open("Visualized/arrays%sboard.txt" %(num), "w")
+    elif type == 2:
+        f = open("Visualized/arrays%srep_astar.txt" %(num),"w")
+        f.write(message)
+    elif type == 3:
+        f = open("Visualized/arrays%sbac_rep_astar.txt" %(num),"w")
+        f.write(message)
+    elif type == 4:
+        f = open("Visualized/arrays%sbackwards.txt" %(num), "w")
+    else:
+        print('No Type Input')
+
     printboard(board, n, f)
     f.close()
 
 def main():
     for i in range(50):
         [board,n,num] = vis_init_s(101,i)
-        visualize(board, n, num)
+        visualize(board, n, num, type = 1)
 
         [board,n,num,message] = vis_init_r(101,i)
-        visualize(board, n, num, message)
+        visualize(board, n, num, message, type = 2)
+
+        [board,n,num,message] = vis_init_r_b(101,i)
+        visualize(board, n, num, message, type = 3)
 main() 
